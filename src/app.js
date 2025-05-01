e97f8eb75a5f6525c6ac0ba27e3011a4f749822e
Deepaksaini2610 <i.m.deepaksaini810787@gmail.com>
2025-05-01 13:59:17 +0530

    add video and user schema

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import e from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = e();
+
+app.use(
+  e.json({
+    limit: "16kb",
+  })
+);
+app.use(cors());
+app.use(
+  e.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+app.use(e.static("public"));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import dotenv from 'dotenv'
-
+import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
-
-dotenv.config(
-    {
-        path:'./env'
-    }
-)
+dotenv.config({
+  path: "./env",
+});
 
 connectDb()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log("server is running at port no.", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failure !!!", error);
+  });
